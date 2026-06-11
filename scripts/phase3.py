8a6809e1f1615db7c7b3f0021f22b51b414cbe2f
Favour Adeleke <fadeleke246@gmail.com>
2026-06-11 12:40:40 +0100

    Update phase3.py

--- scripts/phase3.py
+++ scripts/phase3.py
@@ -7,7 +7,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import config
 from scripts.utils import (
     send_to_admin, get_current_game, update_game_status,
-    set_phase_state    # <-- ADD THIS
+    set_phase_state   # <-- MUST be present
 )
 
 GREYBOX_TEMPLATE = """<!DOCTYPE html>
